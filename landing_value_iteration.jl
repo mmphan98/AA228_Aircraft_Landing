414534b7a82b5d8fdc42c91477ebf163d82de9a5
mphan2468 <mphan2468@gmail.com>
2023-03-03 17:08:36 -0800

    new version of simulator, starting state and action space definitions

--- landing_value_iteration.jl
+++ landing_value_iteration.jl
@@ -1,27 +1,89 @@
-using Printf
-using POMDPs
-include("simulator.jl")
+"""
+This landing_value_iteration.jl file builds an exploration and exploitation model to land a Cessna 172. 
+The model uses Q-learning to find the optimal policy, and and epsilon-greed approach as a means for exploration.
+"""
 
+using Printf
+include("simulator.jl")
 
 # Epsilon Greedy
 
-# QLearning
+# Defining a struct for QLearning
+mutable struct QLearning
+    S       # state space (assumes 1:nstates)
+    A       # action space (assumes 1:nactions)
+    gamma   # discount
+    Q       # action value function
+    alpha   # learning rate
+end
 
+# Defining a function that updates the QLearning model
+function update!(model::QLearning, s, a, r, s_prime)
+    gamma, Q, alpha = model.gamma, model.Q, model.alpha
+    Q[s,a] += alpha*(r + gamma*maximum(Q[s_prime,:]) - Q[s,a])
+    return model
+end
 
+# Defining a function that calculates the Bellman Residual given two Q matrices 
+function BRes(Q, Q_prev)
+    return norm(findmax(Q, dims=2)[1] - findmax(Q_prev, dims=2)[1], Inf) 
+ end
 
 
 """ 
 Airplane Model
+    #Attitude Characteristics
+    x::Float64       # distance of airplane from runway (m)
+    y::Float64       # altitude of airplane from the runway (m)
+    th::Float64      # pitch of the airplane (rad), horizontal is 0
+    power::Float64   # power setting
 
-x::Float64       # distance of airplane from runway (m)
-y::Float64       # altitude of airplane from the runway (m)
-th::Float64      # pitch of the airplane (rad), horizontal is 0
-power::Float64   # power setting, thrust ranges from [20, 200] kg? N?
-V_air::Float64   # airspeed (m/s)
-V_vert::Float64  # vertical airspeed, (m/s)
-alpha::Float64   # angle of path (rad), horizontal is 0
+    #Flight Dynamics
+    V_air::Float64   # airspeed (m/s)
+    alpha::Float64   # angle of path (rad), horizontal is 0
+"""
+#               x     y    th     power V_air alpha
+C172 = Airplane(-4500, 600, 0.075, 150, 40, -0.0525)
+update_Airplane!(C172, 0.1, 100) 
+print(C172)
+print(C172.V_air*cos(C172.alpha))
+print(C172.V_air*sin(C172.alpha))
+
 
 """
-#               x     y    th     power V_air V_vert alpha
-C172 = Airplane(-4500, 600, 0.075, 150, 40, -2.5, -0.0525)
-update_Airplane!(C172, 0.1, 100, 1)
+Definition of state space
+    x --> [-4500, 0] m, mapped to bins of 100m [1 ... 46]
+    y --> [0, 600] m, mapped to bins of 30m [1 ... 21] 
+    x,y --> mapped to [1 ... 1911]
+
+    V_air --> [48, 120] knots, [25, 60] m/s mapped to bins of 1 m/s [1, 36]
+    alpha --> [-5, 5] deg, [-0.08, 0] rad mapped to bins of 0.01 rad [1, 9]
+
+    Total size of state space S = 312,984
+"""
+# Constants
+const x_max = 0
+const x_min = -4500
+const x_step = 100
+
+const y_max = 600
+const y_min = 0
+const y_step = 30
+
+const V_air_max = 60
+const V_air_min = 25
+const V_air_step = 1
+
+const alpha_min = -0.08
+const alpha_max = 0
+
+x = collect(1:46)
+y = collect(1:21)
+
+"""
+Definition of action space
+    6 possible actions
+    pitch up, same, down
+    throttle up, same, down
+    Total size of action space A = 6
+"""
--- simulator.jl
+++ simulator.jl
@@ -3,18 +3,27 @@ This simulator.jl file builds a dynamics model for a Cessna 172 airplane, with a
 and update! function, that will change the struct given new power and pitch settings.
 """
 
+using Random
+using Distributions
+
 #Fundamental Constants
 const roh = 1.225       # density of air, STP, kg/m^3
 const g = 9.81          # gravity, m/s^2
 
 #Plane Constants
-const m = 1157          # mass of airplane (kg)
+const m = 1000          # mass of airplane (kg)
 const A = 16.17         # wing area (m^2)
 const th_max = 0.1745   # max pitch (rad), 20 degrees
 const th_min = -0.1745  # max pitch (rad), -20 degrees
 const power_max = 200   # max power, thrust ranges from [20, 200] N
 const power_min = 20    # min power, thrust ranges from [20, 200] N
 const AOI = 0.0131      # angle of incidence (wing angle of attack, offset from pitch), 1.5 degrees
+
+#Noise Parameters
+const wind_speed = Normal(0,2)
+
+#Model Constants
+const dt = 3            # time step for the dynamics model [s]
 
 """
 Defining a struct for an airplane object
@@ -29,7 +38,6 @@ mutable struct Airplane
 
     #Flight Dynamics
     V_air::Float64   # airspeed (m/s)
-    V_vert::Float64  # vertical airspeed, (m/s)
     alpha::Float64   # angle of path (rad), horizontal is 0
 
 end
@@ -37,45 +45,33 @@ end
 """
 Defining a dynamics model via a function that updates the Airplane model
 """
-function update_Airplane!(model::Airplane, th_p, power_p, dt)
+function update_Airplane!(model::Airplane, th_p, power_p)
 
     # Access existing airplane values from previous step
-    x, y, th, power, V_air, V_vert, alpha = model.x, model.y, model.th, model.power, model.V_air, model.V_vert, model.alpha
+    th, power, V_air, alpha = model.th, model.power, model.V_air, model.alpha
 
     # Updating power
-    if (power_p > power_max)
-        model.power = power_max
-    elseif (power_p < power_min)
-        model.power = power_min
-    else
-        model.power = power_p
-    end
+    model.power = power_p
 
     # Updating angle of path
-    if (th_p > th_max)
-        model.th = th_max
-    elseif (th_p < th_min)
-        model.th = th_min
-    else
-        model.th = th_p
-    end
+    model.th = th_p
 
     # Sum of forces
     lift = Lcoeff(th + AOI) * roh * V_air^2 * A / 2
     drag = Dcoeff(th + AOI) * roh * V_air^2 * A / 2
-    Fx = (power * cos(th)) - (lift * sin(th + AOI)) - (drag * cos(th + AOI))
-    Fy = (-m * g) + (power * sin(th)) + (lift * cos(th + AOI)) - (drag * sin(th + AOI))
+    Fx = (power * g * cos(th)) - (lift * sin(th + AOI)) - (drag * cos(th + AOI))
+    Fy = (-m * g) + (power * g * sin(th)) + (lift * cos(th + AOI)) - (drag * sin(th + AOI))
 
     # Calculate new V_air
     Vx = (V_air * cos(alpha)) + (Fx / m)*dt
     Vy = (V_air * sin(alpha)) + (Fy / m)*dt
+    # Adding Gaussian noise for wind
+    Vx += rand(wind_speed,1)[1]
+    Vy += rand(wind_speed,1)[1]
     model.V_air = sqrt(Vx^2 + Vy^2)
-    
-    # Calculate new V_vert
-    model.V_vert = Vy
 
     # Calculate new alpha, angle of flight path
-    model.alpha = asin(V_vert/V_air)
+    model.alpha = asin(Vy/V_air)
 
     # Calculate new position
     model.x += Vx * abs(cos(alpha)) * dt
@@ -85,15 +81,17 @@ function update_Airplane!(model::Airplane, th_p, power_p, dt)
 end
 
 """
-Defining a function to calculate the drag coefficient, given pitch. Exponential fit
+Defining a function to calculate the drag coefficient, given pitch. Exponential fit vs angle of attack in deg
 """
 function Dcoeff(th)
+    th*=180/pi
     return 0.0178*exp(0.139*th)
 end
 
 """
-Defining a function to calculate the lift coefficient, given pitch. Polynomial fit of degree 6
+Defining a function to calculate the lift coefficient, given pitch. Polynomial fit of degree 6 vs angle of attack in deg
 """
 function Lcoeff(th)
+    th*=180/pi
     return 0.136*(th) - 0.0413*(th^2) + 0.01*(th^3) - 1.01*(10^-3)*(th^4) + 4.59*(10^-5)*(th^5) - 7.69*(10^-6)*(th^6)
 end
