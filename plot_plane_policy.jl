98f7bcd939b7ae3ae9a4e3d47898302b8653536c
Poom Prasopsukh <poom.prasopsukh9@gmail.com>
2023-03-14 13:41:19 -0700

    add plot code

--- plot_plane_policy.jl
+++ plot_plane_policy.jl
@@ -10,14 +10,17 @@ include("data_generation.jl")
 
 
 function plot_policy(inputfilename, outputfilename)
-    inprefix = "E:\\Documents\\2023\\Winter 2023\\Decision Making Under Uncertainty\\AA228_Aircraft_Landing\\policy\\"
-    outprefix = "E:\\Documents\\2023\\Winter 2023\\Decision Making Under Uncertainty\\AA228_Aircraft_Landing\\plots\\"
+    # inprefix = "E:\\Documents\\2023\\Winter 2023\\Decision Making Under Uncertainty\\AA228_Aircraft_Landing\\policy\\"
+    # outprefix = "E:\\Documents\\2023\\Winter 2023\\Decision Making Under Uncertainty\\AA228_Aircraft_Landing\\plots\\"
+    
+    inprefix = "policy/"
+    outprefix = "plots/"
     inputpath = string(inprefix, inputfilename)
     outputpath = string(outprefix, outputfilename)
 
     policy = CSV.read(inputpath, DataFrame; header = false)
 
-    C172 = Airplane(x_min, y_max, 0.00, 150, 50, -0.0525)
+    C172 = Airplane(x_min, y_max, 0.00, 150, 50, -0.0525, false)
     plot_x = Vector{Float64}()
     plot_y = Vector{Float64}()
     plot_th = Vector{Float64}()
@@ -57,6 +60,6 @@ UNCOMMENT TO CREATE NEW PLOTS
 
 Run plotting
 """
-inputfilename = "landing11.policy";
-outputfilename = "testplot11.png";
+inputfilename = "landing13.policy";
+outputfilename = "testplot13.png";
 @time plot_policy(inputfilename, outputfilename)
